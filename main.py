b8a6b722320706eecf7ae99c242e7ad49d7dd1f7
TEAM-FLUFFY <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-02-06 22:15:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ PHOTO_LINK = [
 
 Muhammed = Client(
     "Pyrogram Bot",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"],
+    bot_token ="5205686953:AAG3r6PYveX9LGLlF_JgWvYVSFUzqeDRcWI",
+    api_id ="19143782",
+    api_hash ="43176648b57c393328f832939efb72da"
 )
 
 
@@ -23,7 +23,7 @@ async def start_message(bot, message)
       InlineKeyboardButton("Mo Tech YT", callback_data="start")
       ]]
     await messages.reply_photo(
-        photo=random.choice(PHOTO_LINK),
+        photo=random.choice(https://telegra.ph/file/36e4ef0d26671be24cbc7.jpg),
         text="Hello {message.from_user.mention}   Bro Sugamano",
         reply_markup=InlineKeyboardMarkup(buttons)
     )
